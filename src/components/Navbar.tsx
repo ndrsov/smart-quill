dbd86711acd5910578dd3d213d0e4a9da7026dbc
Andres Soria <andres.zov@gmail.com>
2025-08-12 12:55:52 -0600

    Add buttonVariants import for picing link on Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import MaxWidthWrapper from './MaxWidthWrapper';
+import { buttonVariants } from './ui/button';
 
 const Navbar = () => {
   return (
@@ -7,8 +8,21 @@ const Navbar = () => {
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="flex z-40 font-semibold">
-            <span>Smart Quill</span>
+            <span>Smart Quill.</span>
           </Link>
+
+          {/* todo: add mobila menu  */}
+
+          <div className="hidden items-center space-x-4 sm:flex">
+            <>
+              <Link
+                href={'/pricing'}
+                className={buttonVariants({ variant: 'ghost', size: 'sm' })}
+              >
+                Pricing
+              </Link>
+            </>
+          </div>
         </div>
       </MaxWidthWrapper>
     </nav>
