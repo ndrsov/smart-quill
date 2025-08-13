1d16aa0c2c48606d1ecf9538d074e86058114fed
Andres Soria <andres.zov@gmail.com>
2025-08-13 13:00:26 -0600

    Add Kinde authentication links to Navbar and update dependencies

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import MaxWidthWrapper from './MaxWidthWrapper';
 import { buttonVariants } from './ui/button';
+import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server';
+import { ArrowRight } from 'lucide-react';
 
 const Navbar = () => {
   return (
@@ -21,6 +23,14 @@ const Navbar = () => {
               >
                 Pricing
               </Link>
+              <LoginLink
+                className={buttonVariants({ variant: 'ghost', size: 'sm' })}
+              >
+                Sign In
+              </LoginLink>
+              <RegisterLink className={buttonVariants({ size: 'sm' })}>
+                Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+              </RegisterLink>
             </>
           </div>
         </div>
