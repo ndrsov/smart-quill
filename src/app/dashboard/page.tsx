9c7a2e54e26340e224cfab40b7ce808c0c0458fe
Andres Soria <andres.zov@gmail.com>
2025-08-14 13:22:34 -0600

    Add temporary dashboard page with Kinde authentication session handling

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { redirect } from 'next/navigation';
+
+const Page = () => {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+  console.log(user);
+
+  if (!user || !user.id) redirect('/auth-callback?origin=dashboard');
+  return (
+    <>
+      <div>Dashboard</div>
+    </>
+  );
+};
+
+export default Page;
